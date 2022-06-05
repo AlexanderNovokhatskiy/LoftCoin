f69a929ea546045cee9abf3c0384560330a46cf8
Nyumchik <sfera.2005@mail.ru>
2022-06-05 17:01:53 +0300

    homework 1 & 2 (week1)

---
+++ app/src/main/java/com/loftschool/loftcoin/ui/activity/main/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.loftschool.loftcoin.ui.activity.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.loftschool.loftcoin.R
+import com.loftschool.loftcoin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setSupportActionBar(binding.toolbar)
+        setContentView(binding.root)
+        val navController = findNavController(this, R.id.main_host)
+        setupWithNavController(binding.bottomNav, navController)
+        setupWithNavController(
+            binding.toolbar, navController, AppBarConfiguration.Builder(binding.bottomNav.menu)
+                .build()
+        )
+    }
+}
